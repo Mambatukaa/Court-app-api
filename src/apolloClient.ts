5c5d7828894ec908713b56dce732e85f89216f05
mambatukaa <sbatuka9@gmail.com>
2021-10-29 09:22:00 +0800

    update config in apolloClient

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,35 +1,35 @@
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer, gql, PlaygroundConfig } from 'apollo-server-express';
 import * as dotenv from 'dotenv';
 import resolvers from './data/resolvers';
-import { types, queries, mutations } from './data/schema/court';
-// import resolvers from './data/resolvers';
+import { types, queries, mutations } from './data/schema';
 
 // load environment variables
 dotenv.config();
 
-// const { NODE_ENV } = process.env;
+const { NODE_ENV } = process.env;
 
-// let playground: PlaygroundConfig = false;
+let playground: PlaygroundConfig = false;
 
-// if (NODE_ENV !== 'production') {
-//   playground = {
-//     settings: {
-//       'general.betaUpdates': false,
-//       'editor.theme': 'dark',
-//       'editor.reuseHeaders': true,
-//       'tracing.hideTracingResponse': true,
-//       'editor.fontSize': 14,
-//       'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
-//       'request.credentials': 'include'
-//     }
-//   };
-// }
+if (NODE_ENV !== 'production') {
+  playground = {
+    settings: {
+      'general.betaUpdates': false,
+      'editor.theme': 'dark',
+      'editor.reuseHeaders': true,
+      'tracing.hideTracingResponse': true,
+      'editor.fontSize': 14,
+      'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
+      'request.credentials': 'include'
+    }
+  };
+}
 
 let apolloServer;
 
-export const initApolloServer = () => {
+export const initApolloServer = async () => {
   const typeDefs = gql(`
-      ${types}
+
+    ${types}
     type Query {
       ${queries}
     }
@@ -40,9 +40,23 @@ export const initApolloServer = () => {
 
   apolloServer = new ApolloServer({
     typeDefs,
-    resolvers
-    // playground,
-    // uploads: false,
+    resolvers,
+    playground,
+    uploads: false,
+    context: (req, res) => {
+      const user = req && req.user ? req.user : null;
+
+      const requestInfo = {
+        secure: req.secure,
+        cookies: req.cookies
+      };
+
+      return {
+        user,
+        res,
+        requestInfo
+      };
+    }
   });
 
   return apolloServer;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import * as express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
 import * as dotenv from 'dotenv';
 import { createServer } from 'http';
 import * as compression from 'compression';
 import * as cors from 'cors';
 import { connect } from './db/connection';
-import { types, mutations, queries } from './data/schema';
-import resolvers from './data/resolvers';
 import { getEnv } from './data/utils';
 import userMiddleware from './middlewares/userMiddleware';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
+import { initApolloServer } from './apolloClient';
 
 dotenv.config();
 
@@ -38,39 +36,10 @@ app.use(compression());
 
 app.use(userMiddleware);
 
-const typeDefs = gql`
-  ${types}
-
-  type Query {
-    ${queries}
-  }
-
-  type Mutation {
-    ${mutations}
-  }
-`;
-
 httpServer.listen(PORT, (): void => {
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req, res }) => {
-      const user = req && req.user ? req.user : null;
-
-      const requestInfo = {
-        secure: req.secure,
-        cookies: req.cookies
-      };
-
-      return {
-        user,
-        res,
-        requestInfo
-      };
-    }
-  });
-
-  apolloServer.applyMiddleware({ app, path: '/graphql', cors: corsOptions });
+  initApolloServer().then(apolloServer =>
+    apolloServer.applyMiddleware({ app, path: '/graphql', cors: corsOptions })
+  );
 
   console.log(`\n🚀 GraphQL is now running on ${PORT}`);
 });
